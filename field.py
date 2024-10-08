5f7da631933c95bc28b419050f8c115f95c0154a
yamai sora <s243188@stn.nagaokaut.ac.jp>
2024-10-08 16:58:25 +0900

    field.pyで何をするか

---
+++ field.py
@@ -0,0 +1,14 @@
+class Field:
+    """Fieldクラス
+    Fieldクラスはゲームのフィールドを担うクラスです。
+    フィールドの初期化やディスプレイを行なう。
+    
+    Attributes:
+        players (list[Player]): プレイヤーのリスト
+        enemies (list[Enemy]): 敵のリスト
+        foods (list[Food]]): アイテムのリスト
+        blocks (list[Block]): アイテムのリスト
+        field (list[list[str]]): フィールドの情報
+        f_size (int): フィールドのサイズ
+    """
+    pass
