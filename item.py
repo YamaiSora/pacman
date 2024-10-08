eadd927da3fb4299c1d33a37893d5541e0f5bcbf
s221089 <s221089@stn.nagaokaut.ac.jp>
2024-10-08 17:05:27 +0900

    player.pyを作成

---
+++ item.py
@@ -0,0 +1,44 @@
+"""親クラス
+block,player,enemy,foodの親クラス
+"""
+
+
+class Item:
+    """block,player,enemy,foodの親クラス
+
+    Attributes:
+       now_x(int) : 現在のx座標
+       now_y(int) : 現在のy座標
+       next_x(int) : 次の時刻でのx座標
+       next_y(int) : 次の時刻でのy座標
+       status(bool) : アイテムの状態（Trueなら存在する、Falseなら存在しない消滅した）
+       icon(str) : 表示されるアイテムのアイコン
+    """
+
+    def __init__(self, x, y) -> None:
+        """Itemクラスのコンストラクタ
+        引数にx座標とy座標を受け取り、それぞれの座標を初期化する
+
+        Args:
+            x(int): x座標
+            y(int): y座標
+
+        Returns:
+            None
+
+        Examples:
+            >>> item = Item(2, 3)
+            >>> item.now_x
+            2
+            >>> item.now_y
+            3
+            >>> item.next_x
+            2
+            >>> item.next_y
+            3
+            >>> item.icon
+            ''
+            >>> item.status
+            True
+            """
+        pass
